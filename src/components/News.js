cf5da600bd570f1c25d062e04c239d9fedca5452
JoseIgnacioTapia <josignaciot@gmail.com>
2022-02-18 18:02:41 -0300

    News Api

--- src/components/News.js
+++ src/components/News.js
@@ -1,3 +1,11 @@
+import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
+import moment from 'moment';
+
+import { useGetCryptoNewsQuery } from '../services/crytoNewsApi';
+
+const { Text, Title } = Typography;
+const { Option } = Select;
+
 const News = () => {
   return <div>News</div>;
 };
