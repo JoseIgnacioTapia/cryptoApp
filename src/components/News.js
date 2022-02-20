6ef7d95dc7efc7c1dcd06287cec3cf5731d2d508
JoseIgnacioTapia <josignaciot@gmail.com>
2022-02-20 18:21:33 -0300

    Getting Data - News Component

--- src/components/News.js
+++ src/components/News.js
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
 import moment from 'moment';
 
 import { useGetCryptoNewsQuery } from '../services/crytoNewsApi';
+import { useGetCryptosQuery } from '../services/cryptoApi';
 
 const { Text, Title } = Typography;
 const { Option } = Select;
@@ -10,15 +12,36 @@ const demoImage =
   'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News';
 
 const News = ({ simplified }) => {
+  const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
   const { data: cryptoNews } = useGetCryptoNewsQuery({
-    newsCategory: 'Cryptocurrency',
+    newsCategory: newsCategory,
     count: simplified ? 6 : 12,
   });
+  const { data } = useGetCryptosQuery(100);
 
   if (!cryptoNews?.value) return 'Loading...';
 
   return (
     <Row gutter={[24, 24]}>
+      {!simplified && (
+        <Col span={24}>
+          <Select
+            showSearch
+            className="select-news"
+            placeholder="Select a Crypto"
+            optionFilterProp="children"
+            onChange={value => setNewsCategory(value)}
+            filterOption={(input, option) =>
+              option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+            }
+          >
+            <Option value="Cryptocurrency">Cryptocurrencies</Option>
+            {data?.data?.coins.map(coin => (
+              <Option value={coin.name}>{coin.name}</Option>
+            ))}
+          </Select>
+        </Col>
+      )}
       {cryptoNews.value.map((news, i) => (
         <Col xs={24} sm={12} lg={8} key={i}>
           <Card hoverable className="news-card">
